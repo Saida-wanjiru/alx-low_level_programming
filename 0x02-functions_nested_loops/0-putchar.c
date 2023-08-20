9f169dd4063209705e574534bc448a74912d6b90
Saida-wanjiru <saidawanjiru665@gmail.com>
2023-08-20 12:29:15 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,20 @@
-#include &quot;main.h&quot;
+#include "main.h"
+
 /**
-* main - check the code
-*
-* Return: Always 0.
-*/
+ * main - prints the word _putchar
+ * Return: return 0
+ */
+
 int main(void)
 {
-print_number(98);
-_putchar(&#39;\n&#39;);
-print_number(402);
-_putchar(&#39;\n&#39;);
-print_number(1024);
-_putchar(&#39;\n&#39;);
-print_number(0);
-_putchar(&#39;\n&#39;);
-print_number(-98);
+	char word[9] = "_putchar";
+	int i = 0;
+
+	while (i < 8)
+	{
+		_putchar(word[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
