9cefa5bfba9a5dcde432996a29b515b63d9b83ca
Saida-wanjiru <saidawanjiru665@gmail.com>
2023-08-24 09:24:47 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,53 +1,53 @@
-clude <stdio.h>
-#include <string.h>
+#include "main.h"
 
-void reverseString(char *str) {
-    int length = strlen(str);
-    for (int i = 0; i < length / 2; i++) {
-        char temp = str[i];
-        str[i] = str[length - 1 - i];
-        str[length - 1 - i] = temp;
-    }
+/**
+ * infinite_add -  adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: result
+ * @size_r: result lenght
+ * Return: sum
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	/* local variable declaration */
+	int i = 0, j = 0, k, l = 0, f, s, d = 0;
+
+	while (n1[i] != '\0')
+		i++;
+	while (n2[j] != '\0')
+		j++;
+	if (i > j)
+		l = i;
+	else
+		l = j;
+	if (l + 1 > size_r)
+		return (0);
+	r[l] = '\0';
+	for (k = l - 1 ; k >= 0 ; k--)
+	{
+		i--;
+		j--;
+		if (i >= 0)
+			f = n1[i] - '0';
+		else
+			f = 0;
+		if (j >= 0)
+			s = n2[j] - '0';
+		else
+			s = 0;
+		r[k] = (f + s + d) % 10 + '0';
+		d = (f + s + d) / 10;
+	}
+	if (d == 1)
+	{
+		r[l + 1] = '\0';
+		if (l + 2 > size_r)
+			return (0);
+		while (l-- >= 0)
+			r[l + 1] = r[l];
+		r[0] = d + '0';
+	}
+	return (r);
 }
-
-void infiniteAdd(char *num1, char *num2, char *result, int maxLength) {
-    int carry = 0;
-    int i = 0;
-
-    reverseString(num1);
-    reverseString(num2);
-
-    while (num1[i] != '\0' || num2[i] != '\0' || carry != 0) {
-        int digit1 = num1[i] - '0';
-        int digit2 = num2[i] - '0';
-
-        int sum = digit1 + digit2 + carry;
-        carry = sum / 10;
-        int digitSum = sum % 10;
-
-        result[i] = digitSum + '0';
-
-        i++;
-
-        if (i >= maxLength - 1) {
-            printf("Result is too long for the allocated buffer.\n");
-            return;
-        }
-    }
-
-    result[i] = '\0';
-    reverseString(result);
-}
-
-int main() {
-    char num1[] = "12345678901234567890";
-    char num2[] = "98765432109876543210";
-    char result[100]; // Adjust the buffer size as needed
-
-    infiniteAdd(num1, num2, result, sizeof(result));
-
-    printf("Result: %s\n", result);
-
-    return 0;
-}
-
