548fc4feb52143fb2ed2985dd0b16bb74aab0286
Saida-wanjiru <saidawanjiru665@gmail.com>
2023-09-01 02:34:17 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-
-int main(int argc, char *argv[]) {
-    // Check if there are exactly two arguments
-    if (argc != 3) {
-        printf("Usage: %s arg1 arg2\n", argv[0]);
-        return 1;  // Return an error code
-    }
-
-    // Print the two arguments
-    printf("Argument 1: %s\n", argv[1]);
-    printf("Argument 2: %s\n", argv[2]);
-
-    return 0;  // Return success
+#include <stdlib.h>
+/**
+ *main - prints all arguments recieved
+ *@argc: first parameter
+ *@argv: second parameter
+ *DESCRIPTION: prints all arguments it recieve
+ *Return: Always (0) Success
+ */
+int main(int argc, char **argv)
+{
+while (argc--)
+printf("%s\n", *argv++);
+return (0);
 }
